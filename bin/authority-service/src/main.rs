dff68297bf3b3c63073f3ca7869c12092cf4c73e
Michal Gralinski <michal.gralinski@brightinventions.pl>
2022-09-14 15:01:10 +0200

    Create a microservice that accepts connections from the substrate node.

---
+++ bin/authority-service/src/main.rs
@@ -0,0 +1,27 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use]
+extern crate rocket;
+
+use std::{
+	env,
+	sync::{Arc, Mutex},
+};
+
+mod authority;
+mod config;
+mod handler;
+
+#[launch]
+fn rocket() -> _ {
+	let args: Vec<String> = env::args().collect();
+	let mut data = authority::AuthorityData::new();
+	if let Some(path) = args.last() {
+		if let Some(cfg) = config::Config::from_json_file(path) {
+			data = authority::AuthorityData::create(cfg);
+		}
+	}
+
+	rocket::build()
+		.manage(Arc::new(Mutex::new(data)))
+		.mount("/", routes![handler::authorize, handler::authorize_fix_order])
+}
